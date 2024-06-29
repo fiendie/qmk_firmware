57346f3a7711037d136db13467eec138fde9687e
Andreas Tacke <at@fiendie.net>
2024-06-29 11:40:03 +0200

    Rename/swap settings and media layers, add more transparent keys

--- keyboards/omkbd/ergodash/rev1/keymaps/hacking_friendly/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/hacking_friendly/keymap.c
@@ -4,8 +4,8 @@
 enum layers {
     BASE,
     GERMAN,
-    SETTINGS,
-    MEDIA
+    MEDIA_FN,
+    KB_SETTINGS
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -14,13 +14,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // +--------+--------+--------+--------+--------+--------+--------+                      +--------+--------+--------+--------+--------+--------+--------+
             KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                    RSFT(KC_GRV), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         // |--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
-            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LOPT(KC_8),                  LOPT(KC_9), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_UNDS,
+            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LOPT(KC_8),                  LOPT(KC_9), DE_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_UNDS,
         // |--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
-            KC_RPRN, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOPT(KC_7),                     KC_SLSH, KC_H,    KC_J,    KC_K,    KC_L,    KC_RBRC, KC_BSLS,
+            KC_RPRN, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    LOPT(KC_7),                     DE_MINS, KC_H,    KC_J,    KC_K,    KC_L,    DE_PLUS, KC_BSLS,
         // |--------+--------+--------+--------+--------+--------+--------+                      +--------+--------+--------+--------+--------+--------+--------|
-            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
+            KC_LSFT, DE_Y,    KC_X,    KC_C,    KC_V,    KC_B,                                             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
         // |--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------|
-            KC_LCTL, MO(2),   MO(3),   KC_LALT,          KC_LGUI, KC_SPC,  KC_DEL,       OSL(1),  KC_ENT,  KC_RGUI,          KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+            KC_LCTL, MO(3),   MO(2),   KC_LOPT,          KC_LGUI, KC_SPC,  KC_DEL,       OSL(1),  KC_ENT,  KC_RGUI,          KC_ROPT, KC_LEFT, KC_DOWN, KC_RGHT
         // +--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------+
     ),
 
@@ -38,33 +38,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // +--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------+
     ),
 
-    [SETTINGS] = LAYOUT_3key_2us(
+    [MEDIA_FN] = LAYOUT_3key_2us(
         // +--------+--------+--------+--------+--------+--------+--------+                      +--------+--------+--------+--------+--------+--------+--------+
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,                        BL_TOGG, BL_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
         // |--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMOD,                       RGB_MOD, BL_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         // |--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SPD,                        RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         // |--------+--------+--------+--------+--------+--------+--------+                      +--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_INS,
         // |--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, _______, XXXXXXX, XXXXXXX,          RGB_VAD, RGB_HUD, RGB_HUI,      RGB_SAD, RGB_SAI, RGB_VAI,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+            _______, XXXXXXX, _______, XXXXXXX,          KC_MRWD, KC_MPLY, KC_MUTE,      KC_VOLD, KC_VOLU, KC_MFFD,          XXXXXXX, KC_HOME, KC_PGDN, KC_END
         // +--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------+
     ),
 
-    [MEDIA] = LAYOUT_3key_2us(
+    [KB_SETTINGS] = LAYOUT_3key_2us(
         // +--------+--------+--------+--------+--------+--------+--------+                      +--------+--------+--------+--------+--------+--------+--------+
-            XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG,                        BL_TOGG, BL_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
         // |--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_RMOD,                       RGB_MOD, BL_DOWN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         // |--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SPD,                        RGB_SPI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         // |--------+--------+--------+--------+--------+--------+--------+                      +--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX,
+            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         // |--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, _______, XXXXXXX,          KC_MRWD, KC_MPLY, KC_STOP,      KC_VOLD, KC_VOLU, KC_MFFD,          XXXXXXX, KC_HOME, KC_PGDN, KC_END
+            _______, _______, XXXXXXX, XXXXXXX,          RGB_VAD, RGB_HUD, RGB_HUI,      RGB_SAD, RGB_SAI, RGB_VAI,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
         // +--------+--------+--------+--------+--------+--------+--------+--------+    +--------+--------+--------+--------+--------+--------+--------+--------+
-)};
+    )
+};
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
     const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = { };
